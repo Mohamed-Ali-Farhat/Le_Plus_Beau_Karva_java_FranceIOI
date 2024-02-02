59d6535848821f72057a75ce04785379003a5846
Mohamed Ali Farhat <100702507+MohamedAliFathat@users.noreply.github.com>
2024-02-02 18:26:39 +0000

    solution

---
+++ Le_Plus_Beau_Karva.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Main {
+    static Scanner entrée = new Scanner(System.in);
+    public static void main(String[] args) {
+       
+ int tot=0;
+ int n= entrée.nextInt();
+
+
+
+for (int i=0 ; i<n ;i++){
+
+
+
+
+ int a= entrée.nextInt();
+ int b= entrée.nextInt();
+ int c= entrée.nextInt();
+ int d= entrée.nextInt();
+    
+tot=c*d+a ;
+
+System.out.println(tot);  
+    
+ 
+ 
+      
+}
+
+}
+}
